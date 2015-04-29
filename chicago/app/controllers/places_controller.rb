170724e5ad3b3ed3fe186da2b51f410de4814dbd
ZHAO ZHANG <jocelynz@uchicago.edu>
2015-04-29 00:45:24 -0500

    HW

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -1,43 +1,47 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+   @places = Place.order('title asc')
   end
 
   def show
     @place = Place.find(params[:id])
+    
+    if @place == nil
+    	redirect_to places_url
+    end
   end
 
   def new
   end
 
-  def submitted
+  def create
     Place.create title: params[:title],
                  price: (params[:price].to_f * 100).to_i,
                  photo_url: params[:url],
                  desc: params[:desc]
 
-    redirect_to root_path
+    redirect_to places_url
   end
 
   def edit
-    @place = Place.find(params[:id])
+  	@place = Place.find_by(id: params["id"])
   end
-
+	
   def update
     @place = Place.find(params[:id])
     @place.update title: params[:title],
-                 price: (params[:price].to_f * 100).to_i,
-                 photo_url: params[:url],
-                 desc: params[:desc]
-
-    redirect_to "/places/#{@place.id}"
+                  price: (params[:price].to_f * 100).to_i,
+                  photo_url: params[:url],
+                  desc: params[:desc]
+                  
+    redirect_to place_url(@place.id)
   end
-
+  
   def delete
-    Place.delete(params[:id])
-    redirect_to root_path
+    @place = Place.find_by(id: params["id"])
+    @place.delete
+    redirect_to places_url
   end
-
 
 end
--- chicago/config/routes.rb
+++ chicago/config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
 
   get '/places' => 'places#index'
   get '/places/new' => 'places#new'
-  get '/places/submit_new' => 'places#submitted'
-  get '/places/:id' => 'places#show'
-  get '/places/:id/delete' => 'places#delete'
+  
+  post '/places' => 'places#create'
+  get '/places/:id' => 'places#show', :as => 'place'
+  
+  delete '/places/:id' => 'places#delete' 
   get '/places/:id/edit' => 'places#edit'
-  get '/places/:id/submit_edit' => 'places#update'
+  patch '/places/:id' => 'places#update'
 
-  get '/reviews/:place_id/submit_new'  => 'reviews#create'
+  get '/places/:place_id/reviews' => 'reviews#index', :as => 'reviews'  
+  post '/places/:place_id/reviews'  => 'reviews#create'
 
 end
